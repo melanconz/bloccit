db1d0c92afff47afc0a597fc0398e5a589ac22ac
Zachary Melacon <zmelan01@gmail.com>
2015-10-16 01:19:48 +0000

    Assignment 33 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,10 @@ posts = Post.all
         body: RandomData.random_paragraph
     )
 end
+
+puts "#{Post.count}"
+Post.find_or_create_by(title: "A unique title", body: "A unique body")
+puts "#{Post.count}"
     
 puts "Seed finished"
 puts "#{Post.count} posts created"
