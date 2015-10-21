00be4962c4c82379dfb6eb7cdebbafb8ce1bde58
Zachary Melacon <zmelan01@gmail.com>
2015-10-21 02:03:05 +0000

    assignment 32 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,17 @@ posts = Post.all
     )
 end
     
+50.times do
+    Advertisements.create!(
+        title: RandomData.random_sentence,
+        copy: RandomData.random_paragraph,
+        price: RandomData.random_integer
+    )
+end
+
+advertisements = Advertisement.all
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
