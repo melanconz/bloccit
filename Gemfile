f7ce9b9f30ba0c6cf9ec0d6ae86c2d571795ce3f
Zachary Melacon <zmelan01@gmail.com>
2015-10-21 21:41:45 +0000

    Partial for checkpoint 33

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@
  end
  
  # Use SCSS for stylesheets
- gem 'sass-rails', '~> 5.0'
+ gem 'sass-rails', '>= 5.0'
  # Use Uglifier as compressor for JavaScript assets
  gem 'uglifier', '>= 1.3.0'
  # Use CoffeeScript for .coffee assets and views
@@ -32,6 +32,6 @@
  
  gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
  
- gem 'bootstrap-sass'
+ gem 'bootstrap-sass', '~> 3.3.5'
  
  
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -66,11 +66,45 @@ let (:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomDat
     end
   end
 
-  #describe "GET #edit" do
-  #  it "returns http success" do
-  #    get :edit
-  #    expect(response).to have_http_status(:success)
-  #  end
-  #end
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
 
+    it "renders the #edit view" do
+        get :edit, {id: my_post.id}
+        expect(response).to render_template :edit
+    end
+    
+    it "assigns post to be updated to @post" do
+        get :edit, {id: my_post.id}
+        post_instance = assigns(:post)
+        expect(post_instance.id).to eq my_post.id
+        expect(post_instance.title).to eq my_post.title
+        expect(post_instance.body).to eq my_post.body
+    end
+ end
+
+ describe "PUT update" do
+     it "updates post with expected attributes" do
+         new_title = RandomData.random_sentence
+         new_body = RandomData.random_paragraph
+         
+         put :update, id: my_post.id, post: {title: new_title, body: new_body}
+         
+         update_post = assigns(:post)
+         expect(update_post.id).to eq my_post.id
+         expect(update_post.title).to eq new_title
+         expect(update_post.body).to eq new_body
+     end
+     
+     it "redirects to the updated post" do
+         new_title = RandomData.random_sentence
+         new_body = RandomData.random_paragraph
+         
+         put :update, id: my_post.id, post: {title: new_title, body: new_body}
+         expect(response).to redirect_to my_post
+     end
+ end
 end
