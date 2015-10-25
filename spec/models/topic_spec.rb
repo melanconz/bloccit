67ee16f68825a95d2971bebe1d3c93ff04f59c36
Zachary Melacon <zmelan01@gmail.com>
2015-10-25 21:41:33 +0000

    assignment 35 completed, validations

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   
   it { should have_many(:posts) }
+  
+  it { should validate_length_of(:name).is_at_least(15) }
   describe "attributes" do
       it "should respond to name" do
           expect(topic).to respond_to(:name)
