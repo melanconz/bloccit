5b496ecc71402c5b8ea0af1d8bcf7751bf9167d7
Zachary Melacon <zmelan01@gmail.com>
2015-10-29 03:21:26 +0000

    checkpoint 40 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
  class User < ActiveRecord::Base
    has_many :posts
    before_save { self.email = email.downcase }
-
+   before_save { self.role ||= :member }
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -14,4 +14,6 @@
              format: { with: EMAIL_REGEX }
 
    has_secure_password
+   
+   enum role: [:member, :admin]
  end
