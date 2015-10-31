c4185b81afd71816e6b60a2bb9bc47e4b76fbfbf
Zachary Melacon <zmelan01@gmail.com>
2015-10-31 04:02:42 +0000

    checkpoint 43 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
  class User < ActiveRecord::Base
-   has_many :posts
-   has_many :comments
+   has_many :posts, dependent: :destroy
+   has_many :comments, dependent: :destroy
+   has_many :votes, dependent: :destroy
    before_save { self.email = email.downcase }
    before_save { self.role ||= :member }
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
+   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+   validates :password, length: { minimum: 6 }, allow_blank: true
    validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
