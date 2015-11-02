05f6b3e271758208977ba894494e842eb3fe4468
Zachary Melacon <zmelan01@gmail.com>
2015-11-02 21:53:55 +0000

    fix error

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,5 @@
 module TopicsHelper
     def user_is_authorized_for_topic?
-        current_user && current_user.moderator? && current_user.admin?
+        current_user && (current_user.moderator? || current_user.admin?)
     end
 end
