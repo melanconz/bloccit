00ec86ef0d61535d129bd853c0bb4b3275b5a78e
Zachary Melacon <zmelan01@gmail.com>
2015-11-02 03:30:46 +0000

    checkpoint 48 complete

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,11 @@
    
    get 'about' => 'welcome#about'
    root to: 'welcome#index'
+   
+   namespace :api do
+    namespace :v1 do
+     resources :users, only: [:index, :show]
+     resources :topics, only: [:index, :show]
+    end
+   end
  end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -118,4 +118,10 @@
      end
     end
     
+    describe "#generate_auth_token" do
+     it "creates a token" do
+      expect(user.auth_token).to_not be_nil
+     end
+    end
+    
  end
